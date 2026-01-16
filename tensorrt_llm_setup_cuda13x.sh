f22cfc3b54b8a3a36d7f1a1b37d36aca7226dc1d
hjzccc <junzhouh@usc.edu>
2026-01-16 07:40:51 +0000

    add: pip package installation

--- tensorrt_llm_setup_cuda13x.sh
+++ tensorrt_llm_setup_cuda13x.sh
@@ -1,6 +1,6 @@
 # https://nvidia.github.io/TensorRT-LLM/installation/linux.html
 
-onda create -n tensorrtllm python=3.12 -y
+conda create -n tensorrtllm python=3.12 -y
 conda activate tensorrtllm
 
 # MPI in the Slurm environment
@@ -17,7 +17,7 @@ sudo apt-get -y install libzmq3-dev
 CURRENT_TORCH_VERSION=$(python -c "import torch; print(torch.__version__)")
 echo "torch==$CURRENT_TORCH_VERSION" > /tmp/torch-constraint.txt
 pip install --upgrade pip setuptools && pip install tensorrt_llm -c /tmp/torch-constraint.txt
-
+pip install pytest parameterized
 # When loading shared libraries (OpenMPI), look into conda's lib first!
 # IMPORTANT!!!! BUT CONFLICT W/ GIT, SO USE SEPARATE SHELL FOR GIT
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
