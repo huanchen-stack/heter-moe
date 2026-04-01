0cb8a962c65c8f15fa775cf343c49ca974f5b865
huanchen <84217478+huanchen-stack@users.noreply.github.com>
2026-04-01 15:46:32 -0700

    Update TensorRT LLM setup script for package installation
    Added 'aria2' to the installation of 'libopenmpi-dev' and corrected the 'pip install' command syntax.

--- scripts/tensorrt_llm_setup.sh
+++ scripts/tensorrt_llm_setup.sh
@@ -31,12 +31,12 @@ conda install -c conda-forge mpi4py -y
 
 pip install uv
 uv pip install torch==2.9.0 torchvision --index-url https://download.pytorch.org/whl/cu130
-sudo apt-get -y install libopenmpi-dev
+sudo apt-get -y install libopenmpi-dev aria2
 CURRENT_TORCH_VERSION=$(python -c "import torch; print(torch.__version__)")
 echo "torch==$CURRENT_TORCH_VERSION" > /tmp/torch-constraint.txt
 # pip install --extra-index-url https://pypi.nvidia.com/ "tensorrt-llm==1.1.0" -c /tmp/torch-constraint.txt
 aria2c -x 16 -s 16 "https://pypi.nvidia.com/tensorrt-llm/tensorrt_llm-1.1.0-cp312-cp312-linux_x86_64.whl" -d /tmp/
-pip install /tmp/tensorrt_llm-1.1.0-cp312-cp312-linux_x86_64.whl --extra-index-url https://pypi.nvidia.com/ -c /tmp/torch-constraint.txt
+uv pip install /tmp/tensorrt_llm-1.1.0-cp312-cp312-linux_x86_64.whl --extra-index-url https://pypi.nvidia.com/ -c /tmp/torch-constraint.txt
 
 export PMIX_MCA_psec=native
 export PRTE_MCA_plm_ssh_agent=""
